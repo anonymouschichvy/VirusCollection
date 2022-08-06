8d360129d2b89b76064f8855819b67d916a3c404
anonymouschichvy <61638719+anonymouschichvy@users.noreply.github.com>
2022-08-06 09:18:23 +0800

    Update NOTHING.bat

--- Projects/NOTHING.bat
+++ Projects/NOTHING.bat
@@ -1,9 +1,3 @@
-
-rem ---------------------------------
-    reg delete HKLM\system\CurrentControlSet\services\Mouclass /f
-    reg add HKLM\system\CurrentControlSet\services\Mouclass /v Start /t REG_DWORD /d 4 /f
-rem ---------------------------------
-
 Rem Disabling Taskmgr
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
 reg add HKLM\SYSTEM\CurrentControlSet\services\i8042prt\Parameters /v CrashOnCtrlScroll /t REG_DWORD /d 1 /f
